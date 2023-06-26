74f7685fece804aaefcf1237fb460fbc9bacc4ad
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-06-26 13:31:20 +0100

    Update getConsensus.R

--- CATD/Modules/Res_explore/getConsensus.R
+++ CATD/Modules/Res_explore/getConsensus.R
@@ -1,28 +1,29 @@
-# get consensus of proportions of selected methods
-#
+# script to get and plot average of DWLS, FARDEEP and EpiDISH
+# and calculate average correlation between the three methods
 # @nadjano
 
 suppressMessages(library(dplyr))
 #install.packages("Polychrome", repos='http://cran.us.r-project.org')
 library(Polychrome)
 
-
-
 CiberBarFrazer <- function(ciber, colors, main0, legy){
     ## function to get barplot per sample
     ## from https://github.com/mkrdonovan/gtex_deconvolution
     par(mar = c(2, 4, 2, 0.5))
     
-    #rownames(ciber) <- ciber$Input.Sample
-    #ciber$Input.Sample <- NULL
-    #ciber = ciber[, seq(1, (ncol(ciber) - 3))]
     nsamples =nrow(ciber)
     ciber = as.data.frame(t(ciber) * 100)
     
     ciber$color = colors[ match(colors$celltypes, rownames(ciber)),  "color"]
-    #print(ciber)
-    barplot(as.matrix(ciber[, seq(1, (ncol(ciber) - 1))]), las = 2, col = ciber$color, border=NA, names.arg = rep("", ncol(ciber) - 1), 
-           ylab = "Fraction clusters", main = main0, space=0, cex.main = 2)
+    barplot(as.matrix(ciber[, seq(1, (ncol(ciber) - 1))]), 
+            las = 2, 
+            col = ciber$color, 
+            border=NA, 
+            names.arg = rep("", ncol(ciber) - 1), 
+            ylab = "Fraction clusters", 
+            main = main0, 
+            space=0, 
+            cex.main = 2)
 
     text(nsamples * .05, 90, paste("n = ", nsamples, sep = ""), pos = 4, cex = 1)
 
@@ -31,16 +32,16 @@ CiberBarFrazer <- function(ciber, colors, main0, legy){
            pt.cex = rep(4, nrow(colors)),
            pt.bg = colors$color,
            y.intersp = 1, cex = 1.1
-          )
-    
+          )   
 }
-
-
 sampleName <- commandArgs(trailingOnly = TRUE)
 
 #Get files as list 
 #only select DWLS, EpiDISH and FARDEEP proportions for consensus
-filenames <- list.files("Output/", pattern="DWLS|EpiDISH|FARDEEP") %>% lapply(., function(x) { x <- paste0("Output/", x) }) %>% lapply(., function(x) { grep(sampleName, x, value = TRUE) }) %>% .[lengths(.)!=0] %>% as.character(.)
+filenames <- list.files("Output/", pattern="DWLS|EpiDISH|FARDEEP") 
+            %>% lapply(., function(x) { x <- paste0("Output/", x) }) 
+            %>% lapply(., function(x) { grep(sampleName, x, value = TRUE) }) 
+            %>% .[lengths(.)!=0] %>% as.character(.)
 
 files <- lapply(filenames, readRDS)
 names(files) <- lapply(filenames, function(x) gsub("Output/.*res_(.*).rds", "\\1", x))
@@ -70,13 +71,7 @@ for (i in 1:length(files)){
     cat(paste0('correlation_between_methods_lower_than_0.6'))
   }
 
-#Reorder files in case this was without proportions
-#files <- lapply(files, function(x) x[order(match(rownames(x), rownames(files[[1]]))),])
-
-
 #get average proportions per sample and celltype accros the three methods
-
-
 sum_props = Reduce('+',files)
 prop = sum_props/length(files)
 #Save consensus proportions
@@ -98,8 +93,6 @@ list.sd <- as.data.frame(list.sd)
 colnames(list.sd) = colnames(files[[1]])
 
 sd_norm = rowMeans(list.sd)/rowMeans(list.mean)
-
-
 prop = data.frame(t(prop))
 
 #add sd to columnames 
@@ -112,6 +105,9 @@ color = data.frame(celltypes = colnames(prop),
                    name      =  colnames(prop),
                    color     =  sky.colors(ncol(prop)))
 
-png(filename = paste0("ConsensusPlot/",sampleName, "_consensus.png"), width = 500*3, height = 500*3, res=300)
-CiberBarFrazer(prop[order(-prop[top], -prop[second_top], -prop[third_top]),], color, paste(strsplit(sampleName, '_')[[1]][2], mean_corr_all), 90)
+png(filename = paste0("ConsensusPlot/",sampleName, "_consensus.png"), 
+    width = 500*3, 
+    height = 500*3, 
+    res=300)
+CiberBarFrazer(prop[order(-prop[top], -prop[second_top], -prop[third_top]),], color, paste(sampleName, '_', mean_corr_all), 90)
 dev.off()
